0da6176a98e5b4eaba5f4e20c2d42d05c7dc9b72
Nürek <69650894+Alex8Navas@users.noreply.github.com>
2020-10-21 12:55:57 +0200

    Update NeuralNetworkHeart.R

--- NeuralNetworkHeart.R
+++ NeuralNetworkHeart.R
@@ -4,11 +4,11 @@ library(tidyverse)
 library(ggplot2)
 library(caret)
 
-# emilla sde pseudoaleatorización: 
+# emilla sde pseudoaleatorizaciÃ³n: 
 set.seed(1234)
 
 # Una capa oculta con cinco neuronas. 
-# Funciones de activación a utilizar: tanh y la sigmoide. 
+# Funciones de activaciÃ³n a utilizar: tanh y la sigmoide. 
 
 # Marco de datos a cargar: 
 # https://bmcmedinformdecismak.biomedcentral.com/articles/10.1186/s12911-020-1023-5/tables/1
@@ -25,46 +25,46 @@ head(heart2)
 glimpse(heart2)
 heart2$death <- factor(heart2$DEATH_EVENT, labels = c("Superviviente", "Muerto"))
 
-# Divisón del marco de datos en entrenamiento (80%) y prueba (20%): 
+# DivisÃ³n del marco de datos en entrenamiento (80%) y prueba (20%): 
 index <- round(0.8 * nrow(heart2))
 train <- heart2[1:index, ]
 head(train)
 test <- heart2[(index+1):nrow(heart2), ]
 head(test)
 
-# Vemos si se asemejan los casos de riesgo de fallo cardíaco: 
+# Vemos si se asemejan los casos de riesgo de fallo cardÃ­aco: 
 prop.table(table(train$DEATH_EVENT))
 prop.table(table(test$DEATH_EVENT))
 
 
 ggplot(train, aes(x = death, fill = factor(DEATH_EVENT))) + geom_bar(width = 0.5) +
-  scale_y_continuous("Número de Sujetos", breaks = c(0,25,50,75,100,125,150,175,200), expand=c(0,0)) +
-  scale_fill_manual("Fallo Cardíaco", labels=c("Superviviente","Muerto"), values = c("#B14545", "#4D45B1")) +
-  labs(title = "Supervivencia tras Fallo Cardíaco (Marco de Entrenamiento)",
-       subtitle = "Conjunto de datos que contiene los registros médicos de 239 pacientes que tuvieron insuficiencia cardíaca,
-recogidos durante su período de seguimiento, donde cada perfil de paciente tiene 13 características clínicas.",
+  scale_y_continuous("NÃºmero de Sujetos", breaks = c(0,25,50,75,100,125,150,175,200), expand=c(0,0)) +
+  scale_fill_manual("Fallo CardÃ­aco", labels=c("Superviviente","Muerto"), values = c("#B14545", "#4D45B1")) +
+  labs(title = "Supervivencia tras Fallo CardÃ­aco (Marco de Entrenamiento)",
+       subtitle = "Conjunto de datos que contiene los registros mÃ©dicos de 239 pacientes que tuvieron insuficiencia cardÃ­aca,
+recogidos durante su perÃ­odo de seguimiento, donde cada perfil de paciente tiene 13 caracterÃ­sticas clÃ­nicas.",
        x = "Estado") + 
   theme_light() + theme(legend.position = "bottom",
                         axis.text.x = element_text(angle=55, vjust = 0.5, size = 11))
 
 ggplot(test, aes(x = death, fill = factor(DEATH_EVENT))) + geom_bar(width = 0.5) +
-  scale_y_continuous("Número de Sujetos", breaks = c(0,10,20,30,40), expand=c(0,0)) +
-  scale_fill_manual("Fallo Cardíaco", labels=c("Superviviente","Muerto"), values = c("#B14545", "#4D45B1")) +
-  labs(title = "Supervivencia tras Fallo Cardíaco (Marco de Prueba)",
-       subtitle = "Conjunto de datos que contiene los registros médicos de 60 pacientes que tuvieron insuficiencia cardíaca,
-recogidos durante su período de seguimiento, donde cada perfil de paciente tiene 13 características clínicas.",
+  scale_y_continuous("NÃºmero de Sujetos", breaks = c(0,10,20,30,40), expand=c(0,0)) +
+  scale_fill_manual("Fallo CardÃ­aco", labels=c("Superviviente","Muerto"), values = c("#B14545", "#4D45B1")) +
+  labs(title = "Supervivencia tras Fallo CardÃ­aco (Marco de Prueba)",
+       subtitle = "Conjunto de datos que contiene los registros mÃ©dicos de 60 pacientes que tuvieron insuficiencia cardÃ­aca,
+recogidos durante su perÃ­odo de seguimiento, donde cada perfil de paciente tiene 13 caracterÃ­sticas clÃ­nicas.",
        x = "Estado") + 
   theme_light() + theme(legend.position = "bottom",
                         axis.text.x = element_text(angle=55, vjust = 0.5, size = 11))
 
 
-# Estandarización de las variables numéricas de los marcos de datos:
-# Son age, creatinine_phosphokinase, ejection_fraction, platelets, serum_creatinine, serum_sodium, time.
+# EstandarizaciÃ³n de las variables numÃ©ricas de los marcos de datos:
+# Son age, creatinine_phosphokinase, ejection_fraction, platelets, serum_creatinine, serum_sodium & time.
 
 strain <- data.frame(scale(train[, c("age", "creatinine_phosphokinase", "ejection_fraction", "platelets", "serum_creatinine", "serum_sodium", "time")]))
 
-# Se añaden las variables categóricas: 
-# Son anaemia, diabetes, high_blood_pressure, sex, smoking
+# Se aÃ±aden las variables categÃ³ricas: 
+# Son anaemia, diabetes, high_blood_pressure, sex & smoking.
 strain$anaemia <- train$anaemia
 strain$diabetes <- train$diabetes
 strain$high_blood_pressure <- train$high_blood_pressure
@@ -79,9 +79,9 @@ stest$high_blood_pressure <- test$high_blood_pressure
 stest$sex <- test$sex
 stest$smoking <- test$smoking
 
-# Se guardan las variables DEATH_EVENT de los marcos de entrenamiento y prueba: 
+# Se guardan las variables DEATH_EVENT de los marcos de entrenamiento y prueba. 
 strainDeath <- train$DEATH_EVENT # Vector
-dim(strainDeath) <- c(length(strainDeath), 1) # Redimensionar el vector, añadir una dimensión más. 
+dim(strainDeath) <- c(length(strainDeath), 1) # Redimensionar el vector, aÃ±adir una dimensiÃ³n mÃ¡s. 
 stestDeath <- test$DEATH_EVENT
 dim(stestDeath) <- c(length(stestDeath), 1)
 
@@ -93,16 +93,16 @@ strainDeathmatrix <- t(as.matrix(strainDeath, byrow = TRUE))
 
 
 
-# Construcción de la Red Neuronal 
-# FUnción que define el tamaño de las capas de la red. 
+# ConstrucciÃ³n de la Red Neuronal 
+# FunciÃ³n que define el tamaÃ±o de las capas de la red. 
 getLayerSize <- function(X, y, hidden_neurons, train=TRUE) {
   input <- dim(X)[1]
   hidden <- hidden_neurons
   output <- dim(y)[1]   
   
-  size <- list("input" = input, # Número de neuronas de la capa de entrada. 
-               "hidden" = hidden, # Número de neuronas de la capa oculta.
-               "output" = output) # Número de neuronas de la capa de salida. 
+  size <- list("input" = input, # NÃºmero de neuronas de la capa de entrada. 
+               "hidden" = hidden, # NÃºmero de neuronas de la capa oculta.
+               "output" = output) # NÃºmero de neuronas de la capa de salida. 
   
   return(size)
 }
@@ -111,7 +111,7 @@ getLayerSize <- function(X, y, hidden_neurons, train=TRUE) {
 layerSize <- getLayerSize(strainmatrix, strainDeathmatrix, hidden_neurons = 5)
 layerSize # 12 de entrada, 5 en la oculta y 1 en la salida. 
 
-# Función para inicializar parámetros basándose en una distribución normal. 
+# FunciÃ³n para inicializar parÃ¡metros basÃ¡ndose en una distribuciÃ³n normal. 
 initializeparameters <- function(X, listLayerSize){
   m <- dim(data.matrix(X))[2]
   
@@ -139,17 +139,17 @@ initializeparameters <- function(X, listLayerSize){
 }
 
 
-# Se inicializan los parámetros con la matriz del marco de entrenamiento y el tamaño de la capa. 
+# Se inicializan los parÃ¡metros con la matriz del marco de entrenamiento y el tamaÃ±o de la capa. 
 parameters <- initializeparameters(strainmatrix, layerSize)
-# Observar las dimensiones de los parámetros creados.
+# Observar las dimensiones de los parÃ¡metros creados.
 lapply(parameters, function(x) dim(x))
 
-# Definición de las Funciones de Activación
-# Función sigmoide: 
+# DefiniciÃ³n de las Funciones de ActivaciÃ³n.
+# FunciÃ³n sigmoide.
 sigmoide <- function(x){
   return(1 / (1 + exp(-x)))
 }
-# Función tangente hiperbólica (está en la base de R como tanh(), pero la defino por claridad)
+# FunciÃ³n tangente hiperbÃ³lica (estÃ¡ en la base de R como tanh(), pero la defino por claridad).
 tangentehiperbolica <- function(x){
   return((exp(x) - exp(-x)) / (exp(x) + exp(-x)))
 }
@@ -161,28 +161,28 @@ ysig <- sigmoide(x)
 
 
 plot(x, yth, type = "l", lwd = 3, col = "steelblue",
-     ylab = "Tangente Hiperbólica",
-     main = "Función Tangente Hiperbólica") 
+     ylab = "Tangente HiperbÃ³lica",
+     main = "FunciÃ³n Tangente HiperbÃ³lica") 
 plot(x, ysig, type = "l", lwd = 3, col = "red",
      ylab = "Sigmoide",
-     main = "Función Sigmoide")
+     main = "FunciÃ³n Sigmoide")
 
-# Propagación hacia delante.
+# PropagaciÃ³n hacia delante.
 forwardpropagation <- function(X, params, listLayeSize){
   
   m <- dim(X)[2]
   hidden <- listLayeSize$hidden
   output <- listLayeSize$output
   
-  # Parámetros definidos. 
+  # ParÃ¡metros definidos. 
   W1 <- params$W1
   b1 <- params$b1
   W2 <- params$W2
   b2 <- params$b2
   
   # Redimensionar los interceptos para poder hacer los productos de matrices. 
-  # El inetercepto es el mismo. Se trata de repetir el valor tantas veces como
-  # se lleve a cabo la operación (tantas veces como ecuaciones lineales haya). 
+  # El intercepto es el mismo. Se trata de repetir el valor tantas veces como
+  # se lleve a cabo la operaciÃ³n (tantas veces como ecuaciones lineales haya). 
   b1Redim <- matrix(rep(b1, m), nrow = hidden)
   b2Redim <- matrix(rep(b2, m), nrow = output)
   
@@ -197,31 +197,31 @@ forwardpropagation <- function(X, params, listLayeSize){
                 "A2" = A2)
   return (cache)
 }
-# A2 es el valor que se necesita para la propagación hacia adelante. 
-# El resto de valores que se devuelven serán utilizados para la retropropagación. 
+# A2 es el valor que se necesita para la propagaciÃ³n hacia adelante. 
+# El resto de valores que se devuelven serÃ¡n utilizados para la retropropagaciÃ³n. 
 forwardprop <- forwardpropagation(strainmatrix, parameters, layerSize)
 lapply(forwardprop, function(x) dim(x))
 
-# Definición de la función de Coste: 
-# Concepto entropía cruzada: https://es.wikipedia.org/wiki/Entrop%C3%ADa_cruzada
+# DefiniciÃ³n de la funciÃ³n de Coste: 
+# Concepto entropÃ­a cruzada: https://es.wikipedia.org/wiki/Entrop%C3%ADa_cruzada
 costfunction <- function(X, y, cache) {
   m <- dim(X)[2]
   A2 <- cache$A2
   logprobs <- (log(A2) * y) + (log(1-A2) * (1-y)) 
-  cost <- -sum(logprobs/m) # Binary Cross-Entropy Loss Function (función binaria de pérdida de la entropía cruzada)
+  cost <- -sum(logprobs/m) # Binary Cross-Entropy Loss Function (funciÃ³n binaria de pÃ©rdida de la entropÃ­a cruzada)
   return (cost)
 }
 
 
 # --- Antiguo Script con factor -----
-# Hay que volver a obtener la matriz de la variable categórica como número: 
+# Hay que volver a obtener la matriz de la variable categÃ³rica como nÃºmero: 
 # J <- as.numeric(strainDeathmatrix)
 # J <- if_else(J == 2, 1, 0)
 
 coste <- costfunction(strainmatrix, strainDeathmatrix, forwardprop)
 coste
 
-# Se define la función para llevar a cabo la retropropagación: 
+# Se define la funciÃ³n para llevar a cabo la retropropagaciÃ³n: 
 
 backwardpropagation <- function(X, y, cache, params, listLayerSize){
   
@@ -241,8 +241,8 @@ backwardpropagation <- function(X, y, cache, params, listLayerSize){
   db2_new <- matrix(rep(db2, m), nrow = output)
   
   dZ1 <- (t(W2) %*% dZ2) * (1 - A1^2)
-  dW1 <- 1/m * (dZ1 %*% t(X)) # Derivada de la función de pérdida respecto del peso (W). 
-  db1 <- matrix(1/m * sum(dZ1), nrow = hidden) # Derivada de la función de pérdida respecto del sesgo o bias (b).  
+  dW1 <- 1/m * (dZ1 %*% t(X)) # Derivada de la funciÃ³n de pÃ©rdida respecto del peso (W). 
+  db1 <- matrix(1/m * sum(dZ1), nrow = hidden) # Derivada de la funciÃ³n de pÃ©rdida respecto del sesgo o bias (b).  
   db1_new <- matrix(rep(db1, m), nrow = hidden)
   
   grads <- list("dW1" = dW1, 
@@ -259,12 +259,12 @@ lapply(backprop, function(x) dim(x))
 backprop
 
 
-# Se define una función para la actualización de los pesos. 
-# Parámetros: el gradiente, los parámetros de la red y la ratio de aprendizaje (alfa). 
+# Se define una funciÃ³n para la actualizaciÃ³n de los pesos. 
+# ParÃ¡metros: el gradiente, los parÃ¡metros de la red y la ratio de aprendizaje (alfa). 
 
 updateparameters <- function(grads, params, alpha){
   
-  # Se almacenan los parámetros en variables.
+  # Se almacenan los parÃ¡metros en variables.
   W1 <- params$W1
   b1 <- params$b1
   W2 <- params$W2
@@ -276,7 +276,7 @@ updateparameters <- function(grads, params, alpha){
   dW2 <- grads$dW2
   db2 <- grads$db2
   
-  # Actualización de los pesos. 
+  # ActualizaciÃ³n de los pesos. 
   W1 <- W1 - alpha * dW1
   b1 <- b1 - alpha * db1
   W2 <- W2 - alpha * dW2
@@ -295,13 +295,13 @@ lapply(updated, function(x) dim(x))
 updated
 
 
-# Se define la función de entrenamiento del modelo. 
+# Se define la funciÃ³n de entrenamiento del modelo. 
 
 trainModel <- function(X, y, iterations, hiddenNeurons, alpha){
   
   layerSize <- getLayerSize(X, y, hiddenNeurons)
   parameters <- initializeparameters(X, layerSize)
-  costHistory <- c() # Historial de los costes por iteración. 
+  costHistory <- c() # Historial de los costes por iteraciÃ³n. 
   for (i in 1:iterations) {
     fwdprop <- forwardpropagation(X, parameters, layerSize)
     cost <- costfunction(X, y, fwdprop)
@@ -319,7 +319,7 @@ trainModel <- function(X, y, iterations, hiddenNeurons, alpha){
 }
 
 
-# Se definen las épocas, el número de neuronas de la capa oculta y la ratio de aprendizaje. 
+# Se definen las Ã©pocas, el nÃºmero de neuronas de la capa oculta y la ratio de aprendizaje. 
 epochs = 80000
 hidden = 12
 alfa = 1.1
@@ -329,16 +329,16 @@ RNA <- trainModel(strainmatrix, strainDeathmatrix,
                           iterations = epochs,
                           alpha = alfa)
 
-# Se construye un modelo de regresión logística con glm() para comparar rendimientos.
+# Se construye un modelo de regresiÃ³n logÃ­stica con glm() para comparar rendimientos.
 linealModel <- glm(strainDeath ~., data = strain)
 linealModel
 summary(linealModel)
-# El modelo de regresión lineal se puede mejorar bastante, pero el propósito es compararlo con la RNA, 
-# así que no se estudiará cómo mejorarlo en este script. 
+# El modelo de regresiÃ³n lineal se puede mejorar bastante, pero el propÃ³sito es compararlo con la RNA, 
+# asÃ­ que no se estudiarÃ¡ cÃ³mo mejorarlo en este script. 
 predictione <- round(as.vector(predict(linealModel, stest)))
 predictione
 
-# Se evalúa le rendimiento de la RNA. 
+# Se evalÃºa le rendimiento de la RNA. 
 performanceRNA <- function(X, y, hiddenNeurons){
   layerSize <- getLayerSize(X, y, hiddenNeurons)
   parameters <- RNA$updatedparameters
@@ -366,13 +366,13 @@ tablaRNA
 tablaGLM <- table(as.vector(stestDeath), predictione)
 tablaGLM
 
-# Métricas de Evluación de los modelos:
-# Precisión = TP/(TP+FP)
+# MÃ©tricas de EvluaciÃ³n de los modelos:
+# PrecisiÃ³n = TP/(TP+FP)
 # Recall = = TP/(TP+FN)
-# F1Score = 2*(Precisión*Recall)/(Precisión+Recall)
+# F1Score = 2*(PrecisiÃ³n*Recall)/(PrecisiÃ³n+Recall)
 # Accuracy = (TP+TF)/n
 
-# La tabla obtenida está con las predicciones en la parte superior,
+# La tabla obtenida estÃ¡ con las predicciones en la parte superior,
 # luego tb[3] son los falsos positivos (7 para RNA) y tb[2] son los falsos negativos (5 para RNA)
 calculateStats <- function(tb, model_name) {
   precision <- tb[4]/(tb[4] + tb[3])
@@ -388,6 +388,6 @@ calculateStats <- function(tb, model_name) {
 }
 calculateStats(tablaRNA, "Neural Network")
 calculateStats(tablaGLM, "GLM")
-# Forma rápida de hacerlo con el paquete Caret: 
+# Forma rÃ¡pida de hacerlo con el paquete Caret: 
 confusionMatrix(allespred$RNA, allespred$Real, positive = "Muerto")
 confusionMatrix(allespred$GLM, allespred$Real, positive = "Muerto")
